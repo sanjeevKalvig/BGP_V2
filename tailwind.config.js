19cd0443391221335cce0af39d67b440e0fb982f
sanjeevKalvig <sanjeev.kumar@kalvig.in>
2025-08-25 14:46:32 +0530

    Font style and gamily setup completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        ethnocentric: ['Ethnocentric', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
